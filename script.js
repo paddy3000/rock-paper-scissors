0916a2fd228302dcba4017c5688fa973ada9b21a
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-13 16:02:45 +0000

    feat: Winner determined

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ for (let i=1; i<=1; i++) {
     console.log("##################################")  
 
   //   Human choice
-  let humanChoice=prompt("Please enter Rock, Paper or Scissors");
+  let humanChoice=prompt("Please enter Rock, Paper or Scissors", "");
   console.log("You have chosen: "+humanChoice)
 
   // Computer choice
   let computerChoiceN=Math.floor(Math.random()*3);
   let computerChoice=["Rock", "Paper", "Scissors"][computerChoiceN];
   console.log("Computer has chosen: " + computerChoice);
+
+  // Decide the winner
+  if (humanChoice===computerChoice) {
+    let winner="It's a tie!";
+  } else if (humanChoice==="Rock" && computerChoice=="Scissors" || humanChoice==="Paper" && computerChoice=="Rock" || humanChoice==="Scissors" && computerChoice=="Paper") {
+    winner="You win :)"
+  } else if (humanChoice==="Rock" && computerChoice=="Paper" || humanChoice==="Paper" && computerChoice=="Scissors" || humanChoice==="Scissors" && computerChoice=="Rock") {
+    winner="Unlucky :("
+  } 
+  console.log(winner);
+
 }
