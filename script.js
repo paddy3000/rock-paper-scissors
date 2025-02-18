398bd6face1fc8cfe182206e3002998a2c5fec57
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-18 16:18:45 +0000

    feat: running total changes colour

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ playRound = function(playerChoice) {
 
   // Decide the winner
   const resultsText=document.createElement("p");
+  resultsText.style.fontWeight="bold";
 
   if (playerChoice===computerChoice) {
     resultsText.textContent="It's a tie";
@@ -47,6 +48,7 @@ playAgain = function(button){
     const againButton=document.createElement("button");
     results.appendChild(againButton);
     againButton.textContent="Play again?";
+    againButton.setAttribute("id", "again-button")
 
     againButton.addEventListener("click", function (e) {
         results.removeChild(results.firstChild);
@@ -58,11 +60,19 @@ playAgain = function(button){
     });
 }
 
-// Running total
+// runningTotal function
 runningTotal = function() {
     const runningTotal=document.querySelector(".runningTotal");
     const runningTotalText=document.querySelector(".runningTotalText");
     runningTotalText.textContent=playerName + "'s score: " + playerPoints + ", Paddy's score: " + computerPoints;
+
+    if (playerPoints>computerPoints) {
+        runningTotalText.setAttribute("class", "runningTotalText winning");
+    } else if (playerPoints<computerPoints) {
+        runningTotalText.setAttribute("class", "runningTotalText losing");
+    } else {
+        runningTotalText.setAttribute("class", "runningTotalText drawing");
+    }
 }
 
 // Put everything together
@@ -71,47 +81,11 @@ buttons.forEach((button) => {
         if (letsPlay===true) { 
           e.target.setAttribute("class", "button-clicked"); // Update button formatting
           playerChoice=e.target.getAttribute("id"); // Get playerChoice as variable from button
-          letsPlay=false;
+          letsPlay=false; // Stops players from playing until they press play again
 
-          playRound(playerChoice); // run playRound function
+          playRound(playerChoice);
           playAgain(e.target);
           runningTotal();
         }
     });
   });
-
-// let numberOfRounds=prompt("How many rounds would you like to play?");
-
-// for (let i=1; i<=numberOfRounds; i++) {
-//     // Intro text
-//     console.log("##################################")
-//     console.log("# Round " + i + "                        #")
-//     console.log("##################################")  
-
-//   //   Human choice
-//   let humanChoice=prompt("Please enter Rock, Paper or Scissors", "");
-//   console.log("You have chosen: "+humanChoice)
-
-//   // Computer choice
-//   let computerChoiceN=Math.floor(Math.random()*3);
-//   let computerChoice=["Rock", "Paper", "Scissors"][computerChoiceN];
-//   console.log("Computer has chosen: " + computerChoice);
-
-//   // Decide the winner
-//   if (humanChoice===computerChoice) {
-//     console.log("It's a tie!");
-//   } else if (humanChoice==="Rock" && computerChoice=="Scissors" || humanChoice==="Paper" && computerChoice=="Rock" || humanChoice==="Scissors" && computerChoice=="Paper") {
-//     console.log("You win :)");
-//     humanPoints++;
-//   } else if (humanChoice==="Rock" && computerChoice=="Paper" || humanChoice==="Paper" && computerChoice=="Scissors" || humanChoice==="Scissors" && computerChoice=="Rock") {
-//     console.log("Unlucky :(");
-//     computerPoints++;
-//   } 
-//   console.log("");
-
-//   // Display running total
-//   console.log("Running total:");
-//   console.log("Human points: " + humanPoints + ", Computer points: " + computerPoints);
-//   console.log("");
- 
-// }
