94fd1ecdc427f444220598bece70ff00930ebfe9
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-18 12:10:12 +0000

    feat: Add buttons and style.css sheet

--- script.js
+++ script.js
@@ -4,38 +4,39 @@ console.log("Welcome :)")
 let humanPoints=0;
 let computerPoints=0;
 
-let numberOfRounds=prompt("How many rounds would you like to play?");
 
-for (let i=1; i<=numberOfRounds; i++) {
-    // Intro text
-    console.log("##################################")
-    console.log("# Round " + i + "                        #")
-    console.log("##################################")  
+// let numberOfRounds=prompt("How many rounds would you like to play?");
 
-  //   Human choice
-  let humanChoice=prompt("Please enter Rock, Paper or Scissors", "");
-  console.log("You have chosen: "+humanChoice)
+// for (let i=1; i<=numberOfRounds; i++) {
+//     // Intro text
+//     console.log("##################################")
+//     console.log("# Round " + i + "                        #")
+//     console.log("##################################")  
 
-  // Computer choice
-  let computerChoiceN=Math.floor(Math.random()*3);
-  let computerChoice=["Rock", "Paper", "Scissors"][computerChoiceN];
-  console.log("Computer has chosen: " + computerChoice);
+//   //   Human choice
+//   let humanChoice=prompt("Please enter Rock, Paper or Scissors", "");
+//   console.log("You have chosen: "+humanChoice)
 
-  // Decide the winner
-  if (humanChoice===computerChoice) {
-    console.log("It's a tie!");
-  } else if (humanChoice==="Rock" && computerChoice=="Scissors" || humanChoice==="Paper" && computerChoice=="Rock" || humanChoice==="Scissors" && computerChoice=="Paper") {
-    console.log("You win :)");
-    humanPoints++;
-  } else if (humanChoice==="Rock" && computerChoice=="Paper" || humanChoice==="Paper" && computerChoice=="Scissors" || humanChoice==="Scissors" && computerChoice=="Rock") {
-    console.log("Unlucky :(");
-    computerPoints++;
-  } 
-  console.log("");
+//   // Computer choice
+//   let computerChoiceN=Math.floor(Math.random()*3);
+//   let computerChoice=["Rock", "Paper", "Scissors"][computerChoiceN];
+//   console.log("Computer has chosen: " + computerChoice);
 
-  // Display running total
-  console.log("Running total:");
-  console.log("Human points: " + humanPoints + ", Computer points: " + computerPoints);
-  console.log("");
+//   // Decide the winner
+//   if (humanChoice===computerChoice) {
+//     console.log("It's a tie!");
+//   } else if (humanChoice==="Rock" && computerChoice=="Scissors" || humanChoice==="Paper" && computerChoice=="Rock" || humanChoice==="Scissors" && computerChoice=="Paper") {
+//     console.log("You win :)");
+//     humanPoints++;
+//   } else if (humanChoice==="Rock" && computerChoice=="Paper" || humanChoice==="Paper" && computerChoice=="Scissors" || humanChoice==="Scissors" && computerChoice=="Rock") {
+//     console.log("Unlucky :(");
+//     computerPoints++;
+//   } 
+//   console.log("");
+
+//   // Display running total
+//   console.log("Running total:");
+//   console.log("Human points: " + humanPoints + ", Computer points: " + computerPoints);
+//   console.log("");
  
-}
+// }
